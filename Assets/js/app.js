77bb1ef40b449cd4226297a105b39cf0e6c05e95
DevAndrewJ <ingjeffreysuarez@gmail.com>
2021-08-10 19:16:53 -0400

    Setting arrays

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -5,6 +5,7 @@
 
 
 function startTime() {
+    let groupArra = ["Grupo 1", "Grupo 2", "Grupo 3", "Grupo 4"];
     const today = new Date();
     let h = today.getUTCHours();
     let m = today.getUTCMinutes();
@@ -12,11 +13,7 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-    switch () ( (h >= 00 && h <= 1) && (m <= 35) ) {
-        document.getElementById('group').innerHTML = ("Grupo 1 Activo")
-    }
-    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-    
+    document.getElementById('group').innerHTML = groupArra[0];
     setTimeout(startTime, 1000);
 }
 
@@ -24,13 +21,5 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-
-function activeGroup () {
-
-
-}
-
-
-
 
 startTime();
